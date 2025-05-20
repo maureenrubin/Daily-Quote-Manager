edefaf0d9af6fbb2163db71bf7cf868986acb26d
maureenrubin <maureenrubin21@gmail.com>
2025-05-20 23:24:41 +0800

    Added Common File

---
+++ SimpleCRUDWebApp.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCRUDWebApp.Domain.Commons.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        #region Properties
+
+       // DbSet<User>
+
+        #endregion Properties
+
+    }
+}
