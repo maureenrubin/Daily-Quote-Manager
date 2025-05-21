e524a4bad8c358eba68aea8909153a2ffd3e1d9d
maureenrubin <maureenrubin21@gmail.com>
2025-05-21 11:04:21 +0800

    Add Method at UserConfig

--- SimpleCRUDWebApp.Infastructure/Configurations/UserConfiguration.cs
+++ SimpleCRUDWebApp.Infastructure/Configurations/UserConfiguration.cs
@@ -1,9 +1,20 @@
 ﻿
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SimpleCRUDWebApp.Domain.Entities;
+
 namespace SimpleCRUDWebApp.Infastructure.Configurations
 {
-    public class UserConfiguration 
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        #region Public Methods
 
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+        }
+
+        #endregion Public Methods
     }
 }
