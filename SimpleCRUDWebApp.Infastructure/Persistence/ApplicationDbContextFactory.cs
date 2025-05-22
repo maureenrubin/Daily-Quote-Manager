a521f696366091a01c5884af027af4e5f71547bc
maureenrubin <maureenrubin21@gmail.com>
2025-05-22 20:45:43 +0800

    Added design-time DbContext

--- SimpleCRUDWebApp.Infastructure/Persistence/ApplicationDbContextFactory.cs
+++ SimpleCRUDWebApp.Infastructure/Persistence/ApplicationDbContextFactory.cs
@@ -1,7 +1,39 @@
-﻿namespace SimpleCRUDWebApp.Infastructure.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleCRUDWebApp.Infastructure.Persistence
 {
-    public class ApplicationDbContextFactory
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+   
     {
+        #region Public Methods
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "SimpleCRUDWebApp.Api");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+
+
+
+        #endregion Public Methods
+
 
     }
 }
