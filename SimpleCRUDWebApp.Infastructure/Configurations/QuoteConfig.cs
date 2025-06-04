1697ad0d6bc35bb564de694cbfdaaa554b8319a7
maureenrubin <maureenrubin21@gmail.com>
2025-06-04 21:18:15 +0800

    Added Quote Config

---
+++ SimpleCRUDWebApp.Infastructure/Configurations/QuoteConfig.cs
@@ -0,0 +1,22 @@
+﻿using DailyQuoteManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyQuoteManager.Infrastructure.Configurations
+{
+    public class QuoteConfig : IEntityTypeConfiguration<Quotes>
+    {
+        public void Configure(EntityTypeBuilder<Quotes> builder)
+        { 
+            builder.HasKey(q => q.QuoteId);
+
+            builder.HasOne(q => q.AddedByUser)
+                   .WithMany(u => u.AddedQuotes) 
+                   .HasForeignKey(q => q.AddedByUserId)
+                   .OnDelete(DeleteBehavior.Cascade);  
+
+            builder.HasIndex(q => q.Category);
+
+        }
+    }
+}
