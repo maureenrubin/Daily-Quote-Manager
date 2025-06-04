08330c447cfeac910975b16f70cadc8e8252c683
maureenrubin <maureenrubin21@gmail.com>
2025-06-04 21:19:21 +0800

    Added DbContext Factory

---
+++ SimpleCRUDWebApp.Infastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using DailyQuoteManager.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DailyQuoteManager.Infrastructure
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // Load configuration from appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
