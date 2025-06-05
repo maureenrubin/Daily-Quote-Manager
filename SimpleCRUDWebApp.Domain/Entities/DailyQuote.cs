2b88b86d7fc606e15df8cf143d390ecf1bc4dcfa
maureenrubin <maureenrubin21@gmail.com>
2025-06-05 12:26:57 +0800

    Added Daily Qoute Entity

---
+++ SimpleCRUDWebApp.Domain/Entities/DailyQuote.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DailyQuoteManager.Domain.Entities
+{
+    public class DailyQuote
+    {
+        #region Properties
+
+        [Key]
+        public Guid DailyQuoteId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public Guid QuoteId { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime DateShown { get; set; } = DateTime.UtcNow.Date;
+
+        #endregion
+
+        #region Navigation Properties
+
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; } = null!;
+
+        [ForeignKey(nameof(QuoteId))]
+        public virtual Quotes Quote { get; set; } = null!;
+
+        #endregion
+    }
+}
