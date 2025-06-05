d0251f3f3b8810cb937e1fc6b236c4a9c2cf0eba
maureenrubin <maureenrubin21@gmail.com>
2025-06-05 12:27:45 +0800

    Added Favorite Quote Entity

---
+++ SimpleCRUDWebApp.Domain/Entities/FavoriteQuote.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DailyQuoteManager.Domain.Entities
+{
+    public class FavoriteQuote
+    {
+        #region Properties
+
+        [Key]
+        public Guid FavoriteQuoteId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public Guid QuoteId { get; set; }
+
+        [Required]
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+
+        #endregion
+
+        #region Navigation Properties
+
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; } = null!;
+
+        [ForeignKey(nameof(QuoteId))]
+        public virtual Quotes Quote { get; set; } = null!;
+
+        #endregion
+    }
+}
