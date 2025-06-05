0e1d52e1e98eecaa998acd4f5f68a7be755956da
maureenrubin <maureenrubin21@gmail.com>
2025-06-05 12:24:01 +0800

    Add Config for User

--- SimpleCRUDWebApp.Infastructure/Configurations/ApplicationUserConfig.cs
+++ SimpleCRUDWebApp.Infastructure/Configurations/ApplicationUserConfig.cs
@@ -1,6 +1,7 @@
 ﻿using DailyQuoteManager.Domain.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting.Builder;
 
 namespace DailyQuoteManager.Infrastructure.Configurations
 {
@@ -12,11 +13,29 @@ namespace DailyQuoteManager.Infrastructure.Configurations
         {
             builder.HasKey(u => u.AppUserId);
 
+            //Refresh Token
             builder.HasMany(u => u.RefreshTokens)
-                   .WithOne(r => r.ApplicationUser)
-                   .HasForeignKey(r => r.AppUserId)
+                   .WithOne(r => r.User)
+                   .HasForeignKey(r => r.UserId)
                    .OnDelete(DeleteBehavior.Cascade);
 
+            //Quotes
+            builder.HasMany(u => u.AddedQuotes)
+                   .WithOne(q => q.AddedByUser)
+                   .HasForeignKey(q => q.AddedByUserId)
+                   .OnDelete(DeleteBehavior.SetNull);
+
+            //FavoriteQuotes
+            builder.HasMany(u => u.FavoriteQuotes)
+                   .WithOne(f => f.User)
+                   .HasForeignKey(f => f.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            //DailyQuotes
+            builder.HasMany(u => u.DailyQuotes)
+                   .WithOne(d => d.User)
+                   .HasForeignKey(d => d.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
        
         }
 
