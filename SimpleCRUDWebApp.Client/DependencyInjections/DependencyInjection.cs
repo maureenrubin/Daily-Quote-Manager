bcc3b574b23b3428b6f26953a248bfde28c663e7
maureenrubin <maureenrubin21@gmail.com>
2025-06-06 22:47:07 +0800

    Added DependecyInjection File -client side

---
+++ SimpleCRUDWebApp.Client/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,54 @@
+﻿using DailyQuoteManager.Client.Services;
+using DailyQuoteManager.Client.Services.Contracts;
+using MudBlazor.Services;
+
+namespace DailyQuoteManager.Client.DependencyInjections
+{
+    public static class DependencyInjection
+    {
+        #region Public Methods
+
+        public static IServiceCollection AddServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            AddPersistence(services, configuration);
+
+            return services;
+        }
+
+        #endregion Public Methods 
+
+
+        #region Private Methods 
+        
+        private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient("ApiClient", client =>
+            {
+                // <-- Set this to your running API's base URL + "api/"
+                client.BaseAddress = new Uri("https://localhost:7223/api/");
+            });
+
+            services.AddMudServices();
+            services.AddCascadingAuthenticationState();
+            services.AddHttpContextAccessor();
+
+            services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+
+            services.AddScoped<IAccountManagementClientService, AccountManagementClientService>();
+            services.AddScoped<IAuthClientService, AuthClientService>();
+            services.AddScoped<IQuoteClientService, QuoteClientService>();
+            services.AddScoped<ISnackbarService, SnackbarService>();
+            services.AddScoped<IUserClientService, UserClientService>();
+            services.AddScoped<ITokenClientService, TokenClientService>();
+        }
+
+        #endregion Private Methods 
+
+
+
+    }
+}
