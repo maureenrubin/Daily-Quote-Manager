ede147b6f5463248d5d1acbff42572b4e872923d
maureenrubin <maureenrubin21@gmail.com>
2025-06-07 21:25:22 +0800

    Added ChangePass DTO

---
+++ SimpleCRUDWebApp.Application/DTOs/Auth/ChangePassword/ChangePasswordDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyQuoteManager.Application.DTOs.Auth.ChangePassword
+{
+    public class ChangePasswordDto
+    {
+        #region Properties
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        #endregion Properties
+
+    }
+}
