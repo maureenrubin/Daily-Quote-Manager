13ef3e4d9cb64c70c1d46e261d28608e2dd8cce6
maureenrubin <maureenrubin21@gmail.com>
2025-06-07 21:27:07 +0800

    Added Refresh Token DTO

---
+++ SimpleCRUDWebApp.Application/DTOs/Auth/RefreshToken/RefreshTokenDto.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyQuoteManager.Application.DTOs.Auth.RefreshToken
+{
+    public class RefreshTokenDto
+    {
+        #region Properties 
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime? Expires { get; set; }
+
+        public bool Enable { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        #endregion Properties
+
+    }
+}
