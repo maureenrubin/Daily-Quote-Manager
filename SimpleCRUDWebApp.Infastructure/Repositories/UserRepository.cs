1384f549da3094a74b597b83c2febc1027c8837d
maureenrubin <maureenrubin21@gmail.com>
2025-06-07 21:31:32 +0800

    Added Public Method

--- SimpleCRUDWebApp.Infastructure/Repositories/UserRepository.cs
+++ SimpleCRUDWebApp.Infastructure/Repositories/UserRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace DailyQuoteManager.Infrastructure.Repositories
+﻿using DailyQuoteManager.Domain.Entities;
+using DailyQuoteManager.Domain.Interfaces;
+using DailyQuoteManager.Infrastructure.Data;
+
+namespace DailyQuoteManager.Infrastructure.Repositories
 {
-    class UserRepository
+    public class UserRepository(AppDbContext _dbContext): IUserRepository 
     {
+
+        #region Fields
+
+        #endregion Fields
+
+        #region Public Methods
+        public async Task AddAsync(ApplicationUser user)
+        {
+            // Add the user
+            await _dbContext.ApplicationUsers.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+        }
+
+        #endregion Public Methods
     }
 }
