6cd59e7a310247fc0dd69341a7719c898a759559
maureenrubin <maureenrubin21@gmail.com>
2025-06-09 19:45:32 +0800

    Added I-RefreshToken

--- SimpleCRUDWebApp.Domain/Interfaces/IRefreshTokenRepository.cs
+++ SimpleCRUDWebApp.Domain/Interfaces/IRefreshTokenRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace DailyQuoteManager.Domain.Interfaces
+﻿
+using DailyQuoteManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyQuoteManager.Domain.Interfaces
 {
     public interface IRefreshTokenRepository
     {
+        #region Public Methods 
+
+        public async Task<RefreshToken?> AddAsync(RefreshToken refreshToken, Guid appUserId)
+        {
+            refreshToken.UserId = appUserId;
+
+            await dbContext.RefreshTokens.AddAsync(refreshToken);
+            await dbContext.SaveChangesAsync();
+            return refreshToken;
+        }
+
+        #endregion Public Methods
+
     }
 }
