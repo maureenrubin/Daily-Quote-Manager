dbd91331192a1fe1d2263a60161b969b904ba1b5
maureenrubin <maureenrubin21@gmail.com>
2025-06-11 22:01:54 +0800

    Added AuthResponse DTO

---
+++ SimpleCRUDWebApp.Application/Common/Responses/AuthResponseDto.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyQuoteManager.Application.Common.Responses
+{
+    public class AuthResponseDto
+    {
+
+        #region Properties
+
+        public string AccessToken { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public DateTime TokenExpired { get; set; } = DateTime.UtcNow;
+
+        #endregion Properties
+    }
+}
