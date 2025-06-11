7222df33b92e813572673812d75c361ef98b5e76
maureenrubin <maureenrubin21@gmail.com>
2025-06-11 21:50:20 +0800

    Add Register Method

--- SimpleCRUDWebApp.Application/Services/AuthService.cs
+++ SimpleCRUDWebApp.Application/Services/AuthService.cs
@@ -1,8 +1,54 @@
-﻿using DailyQuoteManager.Application.Services.Contracts;
+﻿using DailyQuoteManager.Application.DTOs.Auth.Register;
+using DailyQuoteManager.Application.Services.Interfaces;
+using DailyQuoteManager.Domain.Entities;
+using DailyQuoteManager.Domain.Interfaces;
+using Microsoft.Extensions.Logging; 
 
 namespace DailyQuoteManager.Application.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService (IUserRepository userRepository,
+                              IRefreshTokenRepository refreshTokenRepository,
+                              IPasswordHasherService passwordHasherService,
+                              IUnitOfWork unitOfWork,
+                              ITokenService tokenService,
+                              ILogger<AuthService> logger) : IAuthService
     {
+
+        #region Public Methods
+
+        public async Task<(bool Success, string Message)> RegisterUserAsync(RegisterUserInputRequestDto request)
+        {
+            var existingUser = await userRepository.GetByEmailAsync(request.Email);
+            if (existingUser != null)
+            {
+                logger.LogWarning("Registration Failed: Email already exists.");
+                return (false, "Email already exists.");
+            }
+
+            var hashedPassword = passwordHasherService.HashPassword(request.Password);
+
+            var roleToAssign = string.IsNullOrWhiteSpace(request.Role) ? "User" : request.Role;
+
+            var newUser = new ApplicationUser
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                PasswordHash = hashedPassword, 
+                CreatedAt = DateTime.UtcNow,
+                Role = roleToAssign
+            };
+
+            await userRepository.AddAsync(newUser);
+            await unitOfWork.SaveChangesAsync();
+
+            logger.LogInformation("User registered successfully with role: {Role}", roleToAssign);
+
+            return (true, $"Registration successful as {roleToAssign}.");
+        }
+
+
+        #endregion Public Methods
+
+
     }
 }
