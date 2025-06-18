fbe423140bab109d2eae349f9cc1681749d666ff
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 00:27:30 +0800

    Remove Role Property

--- SimpleCRUDWebApp.Application/Common/Responses/AuthResponseDto.cs
+++ SimpleCRUDWebApp.Application/Common/Responses/AuthResponseDto.cs
@@ -7,8 +7,6 @@
 
         public string AccessToken { get; set; } = string.Empty;
 
-        public string Token { get; set; } = string.Empty;
-
         public string Email { get; set; } = string.Empty;
 
         public string Role { get; set; } = string.Empty;
