4dc8ac24e46a0eeb950cbb429faa8eb4fad21bdb
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 00:59:43 +0800

    Added migrations for refreshtoken entity

---
+++ SimpleCRUDWebApp.Infastructure/Migrations/20250618145554_AddRefreshTokenFields.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyQuoteManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_ApplicationUsers_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "RefreshTokens",
+                newName: "AppUserId");
+
+            migrationBuilder.RenameColumn(
+                name: "IsRevoked",
+                table: "RefreshTokens",
+                newName: "Enable");
+
+            migrationBuilder.RenameColumn(
+                name: "TokenId",
+                table: "RefreshTokens",
+                newName: "RefreshTokenId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_AppUserId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_ApplicationUsers_AppUserId",
+                table: "RefreshTokens",
+                column: "AppUserId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "AppUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_ApplicationUsers_AppUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "Enable",
+                table: "RefreshTokens",
+                newName: "IsRevoked");
+
+            migrationBuilder.RenameColumn(
+                name: "AppUserId",
+                table: "RefreshTokens",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenId",
+                table: "RefreshTokens",
+                newName: "TokenId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_AppUserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_ApplicationUsers_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "AppUserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
