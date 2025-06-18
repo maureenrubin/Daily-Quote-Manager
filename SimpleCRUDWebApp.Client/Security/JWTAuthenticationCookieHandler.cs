c797f93e7b9b499c859b30b669be8e8c2d4323c0
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 00:58:55 +0800

    Added 2 classes for JWTAuthentication and JWTHttpMessage - handlers

---
+++ SimpleCRUDWebApp.Client/Security/JWTAuthenticationCookieHandler.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DailyQuoteManager.Client.Security
+{
+    public class JWTAuthenticationCookieHandler : AuthenticationHandler<CustomOption>
+    {
+        #region Public Constructors
+        public JWTAuthenticationCookieHandler(
+        IOptionsMonitor<CustomOption> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        TimeProvider time) : base(options, logger, encoder)
+        {
+
+        }
+
+        #endregion Public Constructors
+
+
+        #region Protected Methods
+
+        protected override async Task <AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var token = Request.Cookies["access_token"];
+            if (string.IsNullOrEmpty(token))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            var readJWT = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            var identity = new ClaimsIdentity(readJWT.Claims, "JWT");
+            var principals = new ClaimsPrincipal(identity);
+
+            var ticket = new AuthenticationTicket(principals, Scheme.Name);
+            return AuthenticateResult.Success(ticket);
+
+        }
+
+        protected override Task HandleChallengeAsync (AuthenticationProperties properties)
+        {
+            Response.Redirect("/");
+            return Task.CompletedTask;
+        }
+
+        protected override Task HandleForbiddenAsync (AuthenticationProperties propeties)
+        {
+            Response.Redirect("/notfound404");
+            return Task.CompletedTask;
+        }
+
+        #endregion Protected Methods
+
+    }
+
+
+    public class CustomOption : AuthenticationSchemeOptions
+    {
+
+    }
+}
---
+++ SimpleCRUDWebApp.Client/Security/JwtHttpMessageHandler.cs
@@ -0,0 +1,40 @@
+﻿using DailyQuoteManager.Client.InterfacesClient.Auth;
+using System.Net.Http.Headers;
+namespace DailyQuoteManager.Client.Security
+{
+    public class JwtHttpMessageHandler : DelegatingHandler
+    {
+        #region Fields
+        private readonly ITokenClientService _tokenService;
+        #endregion Fields
+
+        #region Public Constructors
+        public JwtHttpMessageHandler(ITokenClientService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+        #endregion Public Constructors
+
+        #region Protected Methods
+
+        
+
+        
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var token = await _tokenService.GetToken();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+
+        }
+        #endregion Protected Methods
+
+    }
+}
