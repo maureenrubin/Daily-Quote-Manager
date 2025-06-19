a95b92a9526a3f352bb2e201e9430c407e681337
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 23:45:01 +0800

    Creates a file for Repository injection

---
+++ SimpleCRUDWebApp.Infastructure/DependencyInjections/RepositoryInjection.cs
@@ -0,0 +1,30 @@
+﻿using DailyQuoteManager.Application.Contracts.Persistence;
+using DailyQuoteManager.Domain.Interfaces;
+using DailyQuoteManager.Infrastructure.Migrations.Repositories;
+using DailyQuoteManager.Infrastructure.Persistence.Repositories;
+using DailyQuoteManager.Infrastructure.Persistence.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyQuoteManager.Infrastructure.DependencyInjections
+{
+    public static class RepositoryInjection
+    {
+        #region Public Methods
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            return services;
+        }
+
+
+        #endregion Public Methods
+
+
+    }
+}
