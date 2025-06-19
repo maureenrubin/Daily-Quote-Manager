c8d25a4ed66f163395325f66b047cef0dae6ac25
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 23:43:19 +0800

    Creates interface for refresh token service -client side

---
+++ SimpleCRUDWebApp.Client/InterfacesClient/Auth/IRefreshTokenClientService.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyQuoteManager.Client.InterfacesClient.Auth
+{
+    public interface IRefreshTokenClientService
+    {
+        Task Set(string value);
+
+        Task<string> Get();
+
+        Task Remove();
+
+    }
+}
