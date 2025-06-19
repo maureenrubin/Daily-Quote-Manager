b8437621c11ce1170afaf993a50cdd71593a26c2
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 23:44:26 +0800

    Creates method for Refresh Token Service -client side

---
+++ SimpleCRUDWebApp.Client/ServicesClient/Auth/RefreshTokenClientService.cs
@@ -0,0 +1,40 @@
+﻿using DailyQuoteManager.Client.InterfacesClient.Auth;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace DailyQuoteManager.Client.ServicesClient.Auth
+{
+    public class RefreshTokenClientService(
+        ProtectedLocalStorage protectedLocalStorage): IRefreshTokenClientService
+    {
+        #region Fields
+
+        public readonly string key = "refresh_token";
+
+        #endregion Fields
+
+        #region Public Methods
+
+        public async Task Set(string value)
+        {
+            await protectedLocalStorage.SetAsync(key, value);
+        }
+
+        public async Task<string> Get()
+        {
+            var result = await protectedLocalStorage.GetAsync<string>(key);
+            if (result.Success)
+                return result.Value!;
+
+            return string.Empty;
+        }
+
+        public async Task Remove()
+        {
+            await protectedLocalStorage.DeleteAsync(key);
+        }
+
+        #endregion Public Methods
+
+
+    }
+}
