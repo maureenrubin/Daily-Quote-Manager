78a00daac1a345c381893e2dd450f1cbdd090a0b
maureenrubin <maureenrubin21@gmail.com>
2025-06-19 23:41:04 +0800

    Added Persistence File for Interface of Base Entity

---
+++ SimpleCRUDWebApp.Application/Contracts/Persistence/IBaseRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace DailyQuoteManager.Application.Contracts.Persistence
+{
+    public interface IBaseRepository<T>
+    {
+        #region Public Methods
+
+        //retruns single record by id 
+        Task<T?> GetByIdAsync(Guid id);
+
+        //returns all records of what type of entity
+        Task<IEnumerable<T>> ListAllAsync();
+
+        //Add single entity to the database context 
+        Task AddAsync(T entity);
+
+        //Add multiple entities at once
+        Task AddRangeAsync(IEnumerable<T> entities);
+        
+        //Only marks the data as changed, can be changed actually to the db using UnitofWork
+        void Update(T entity);
+
+        //removes an entity from context
+        void Delete(T entity);
+
+        //removes an entity by its id
+        Task<T?> DeleteByIdAsync(Guid id);
+
+        #endregion Public Methods
+
+
+    }
+}
