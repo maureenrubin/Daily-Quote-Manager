a2eff8013ac09b65563b775b9883ca926c70687e
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:37:19 +0800

    Added interface for cookieClientService

---
+++ SimpleCRUDWebApp.Client/InterfacesClient/Auth/ICookieClientService.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyQuoteManager.Client.InterfacesClient.Auth
+{
+    public interface ICookieClientService
+    {
+        #region Public Methods
+
+        Task<string> GetCookies(string key);
+
+        Task RemoveCookies(string key);
+
+        Task SetCookies(string key, string value, int days);
+
+        #endregion Public Methods
+
+    }
+}
