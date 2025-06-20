6332bda684d9e51d945a12c5082395ffacc47e74
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:38:03 +0800

    Creates a method for CookieClientService

---
+++ SimpleCRUDWebApp.Client/ServicesClient/Auth/CookieClientService.cs
@@ -0,0 +1,46 @@
+﻿using DailyQuoteManager.Client.InterfacesClient.Auth;
+using Microsoft.JSInterop;
+
+namespace DailyQuoteManager.Client.ServicesClient.Auth
+{
+    #region Public Methods
+    public class CookieClientService(IJSRuntime jSRuntime) : ICookieClientService
+    {
+        public async Task<string> GetCookies(string key)
+        {
+            try
+            {
+                return await jSRuntime.InvokeAsync<string>("getCookie", key);
+            }
+            catch (InvalidOperationException)
+            {
+                return null!;
+            }
+        }
+
+        public async Task RemoveCookies(string key)
+        {
+            try
+            {
+                await jSRuntime.InvokeVoidAsync("deleteCookie", key);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        public async Task SetCookies(string key, string value, int days)
+        {
+            try
+            {
+                await jSRuntime.InvokeVoidAsync("setCookie", key);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+    }
+
+    #endregion Public Methods
+
+}
