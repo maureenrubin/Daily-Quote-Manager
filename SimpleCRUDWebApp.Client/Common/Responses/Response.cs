b8f2326b3e92964aa092e98a0df2da4fb396db2c
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:34:40 +0800

    Added Response for Login

---
+++ SimpleCRUDWebApp.Client/Common/Responses/Response.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DailyQuoteManager.Client.Common.Responses
+{
+    public record Response(
+        bool IsSuccess = false,
+        bool Success = false,
+        string ErrorMessage = "",
+        string Message = "",
+        string MessageType = "",
+        object? Data = null,
+        Dictionary<string, string[]>? ValidationErrors = null
+        );
+}
