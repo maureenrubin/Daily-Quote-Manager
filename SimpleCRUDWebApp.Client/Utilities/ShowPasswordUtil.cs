150dd5c5ff27ad3b1cfe76970b385766696beec5
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:39:02 +0800

    Added a file and method for ShowPassword

---
+++ SimpleCRUDWebApp.Client/Utilities/ShowPasswordUtil.cs
@@ -0,0 +1,31 @@
+﻿using MudBlazor;
+
+namespace DailyQuoteManager.Client.Utilities
+{
+    public class ShowPasswordUtil
+    {
+        #region Properties
+        public InputType InputType { get; private set; } = InputType.Password;
+        public string Icon { get; private set; } = Icons.Material.Filled.VisibilityOff;
+        public bool IsPasswordVisible => InputType == InputType.Text;
+        #endregion Properties
+
+        #region Public Methods
+
+        public void Toggle()
+        {
+            if(InputType == InputType.Password)
+            {
+                InputType = InputType.Text;
+                Icon = Icons.Material.Filled.Visibility;
+            }
+            else
+            {
+                InputType = InputType.Password;
+                Icon = Icons.Material.Filled.VisibilityOff;
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
