bd2d078995a4efc1b8208894a8802216823930de
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:36:52 +0800

    Added DTOs File -client side

---
+++ SimpleCRUDWebApp.Client/DTOs/Auth/Login/LoginInputRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyQuoteManager.Client.DTOs.Auth.Login
+{
+    public class LoginInputRequestDto
+    {
+        #region Properties
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        #endregion Properties
+    }
+}
---
+++ SimpleCRUDWebApp.Client/DTOs/Auth/Register/RegisterUserInputRequestDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyQuoteManager.Client.DTOs.Auth.Register
+{
+    public class RegisterUserInputRequestDto
+    {
+        #region Properties
+        [Required(ErrorMessage = "Full name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Full name must be between 2 to 100 characters")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be atleast 6 characters")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$",
+             ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")]
+        public string Password { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Gender is required")]
+        [RegularExpression("^(Male|Female|Others)$", ErrorMessage = "Gender must be Male, Female, or Other")]
+        public string Gender { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string Role { get; set; } = string.Empty;
+
+        #endregion Properties
+
+    }
+}
