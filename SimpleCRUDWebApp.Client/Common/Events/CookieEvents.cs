6f47255c877474b507b9d251b636f81abb6b10d6
maureenrubin <maureenrubin21@gmail.com>
2025-06-20 23:34:19 +0800

    Added CookieEvents

---
+++ SimpleCRUDWebApp.Client/Common/Events/CookieEvents.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace DailyQuoteManager.Client.Common.Events
+{
+    public class CookieEvents : CookieAuthenticationEvents
+    {
+        #region Public Methods
+
+        public override Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            context.RedirectUri = "/login";
+            return base.RedirectToLogin(context);
+        }
+
+
+        #endregion Public Methods
+    }
+}
