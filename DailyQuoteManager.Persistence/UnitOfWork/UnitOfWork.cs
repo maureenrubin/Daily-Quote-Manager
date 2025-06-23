c4931f34d744c754b2658ea904f98ab1a87da384
maureenrubin <maureenrubin21@gmail.com>
2025-06-23 23:58:48 +0800

    Create a new persistence class library for UnitOfWork

---
+++ DailyQuoteManager.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using DailyQuoteManager.Domain.Interfaces;
+using DailyQuoteManager.Persistence.DatabaseContext;
+
+namespace DailyQuoteManager.Persistence.UnitOfWork
+{
+    public class UnitOfWork(AppDbContext dbContext,
+                            IUserRepository Users,
+                            IRefreshTokenRepository RefreshTokenRepository): IUnitOfWork
+    {
+        #region Properties
+
+        public IUserRepository Users { get; } = Users;
+        public IRefreshTokenRepository RefreshTokenRepository { get; } = RefreshTokenRepository;
+
+        #endregion Properties
+
+
+        #region Public Methods
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+
+        #endregion Public Methods
+    }
+}
