a43d51c3c4297ce4e4d89a685a072708f06a2105
maureenrubin <maureenrubin21@gmail.com>
2025-06-23 23:55:38 +0800

    Create a new persistence library for entity config

---
+++ DailyQuoteManager.Persistence/Configurations/QuoteConfiguration/QuoteConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DailyQuoteManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyQuoteManager.Persistence.Configurations.QuoteConfiguration
+{
+    public class QuoteConfig : IEntityTypeConfiguration<Quotes>
+    {
+        #region Public Methods
+        
+        public void Configure(EntityTypeBuilder<Quotes> builder)
+        {
+            builder.HasKey(q => q.QuoteId);
+
+            //Quote added by user
+            builder.HasOne(q => q.AddedByUser)
+                   .WithMany(u => u.AddedQuotes)
+                   .HasForeignKey(q => q.AddedByUserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.HasIndex(q => q.Category);
+
+        }
+
+        #endregion
+
+    }
+}
