edae819198974407212cc4cdaee7898897c173ce
maureenrubin <maureenrubin21@gmail.com>
2025-06-23 23:56:27 +0800

    Create a new persistence class library for DBContext

---
+++ DailyQuoteManager.Persistence/DatabaseContext/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DailyQuoteManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyQuoteManager.Persistence.DatabaseContext
+{
+    public class AppDbContext : DbContext
+    {
+        #region Public Constructors
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region DbSets / Properties
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+        public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
+        public DbSet<Quotes> Quotes { get; set; } = null!;
+        public DbSet<FavoriteQuote> FavoriteQuotes { get; set; } = null!;
+        public DbSet<DailyQuote> DailyQuotes { get; set; } = null!;
+
+        #endregion DbSets / Properties
+
+        #region Protected Methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+        #endregion Protected Methods
+    }
+}
