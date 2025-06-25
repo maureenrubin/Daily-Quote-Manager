8536f4ca642e303cad90c59337be6ee156e39ffd
maureenrubin <maureenrubin21@gmail.com>
2025-06-25 23:30:33 +0800

    Creates new DI for Application Services

---
+++ SimpleCRUDWebApp.Application/DependencyInjections/ApplicationServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using DailyQuoteManager.Application.Common.Mapping;
+using DailyQuoteManager.Application.Contracts.Interfaces.Auth;
+using DailyQuoteManager.Application.Contracts.Interfaces.UserManagement;
+using DailyQuoteManager.Application.Services.Auth;
+using DailyQuoteManager.Application.Services.UserManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyQuoteManager.Application.DependencyInjections
+{
+    public static class ApplicationServiceRegistration
+    {
+        #region Public Methods
+
+        public static IServiceCollection AddApplicationService(
+            this IServiceCollection service, IConfiguration configuration)
+        {
+
+            service.AddAutoMapper(typeof(BaseMappingProfiles).Assembly);
+
+            service.AddScoped<IUserService, UserService>();
+            service.AddScoped<IAuthService, AuthService>();
+            service.AddScoped<ITokenService, TokenService>();
+            service.AddScoped<IPasswordHasherService, PasswordHasherService>();
+            service.AddScoped<IRefreshTokenService, RefreshTokenService>();
+
+            return service;
+        }
+
+        #endregion Public Methods
+
+
+
+    }
+}
