792407aa9e863f59354e4b535ac1a83e9beb5cfa
maureenrubin <maureenrubin21@gmail.com>
2025-06-25 23:33:49 +0800

    Applies Production Startup Setup

--- SimpleCRUDWebApp.Server/Program.cs
+++ SimpleCRUDWebApp.Server/Program.cs
@@ -5,83 +5,20 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System.Text;
+using DailyQuoteManager.Api;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
-
-//Add OpenAPI and Swagger for documentation
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerAuth();
-
-//Add JWT Authentication
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.IncludeErrorDetails = true;
-        options.RequireHttpsMetadata = true;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:SecretKey"]!)),
-
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["JWTSettingds:Issuer"],
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JWTSettings:Audience"],
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            RoleClaimType = ClaimTypes.Role
-        };
-    });
-
-builder.Services.AddServices(builder.Configuration);
-
-
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false)
     .Build();
 
 
-// CORS Configuration
-builder.Services.AddCors(policy =>
-{
-    policy.AddPolicy("AllowBlazorApp", policy =>
-    {
-        policy.WithOrigins("https://localhost:7053")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-    });
-});
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeLine();
 
 
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseStaticFiles();
-app.UseCors("AllowBlazorApp");
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseMiddleware<ExceptionMiddleware>();
-app.MapControllers();
 
 app.Run();
