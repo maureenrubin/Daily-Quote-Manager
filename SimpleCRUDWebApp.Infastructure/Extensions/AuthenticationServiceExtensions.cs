98fbf2ccb09d0c08fd1322d11fb492687458d36f
maureenrubin <maureenrubin21@gmail.com>
2025-06-25 23:31:06 +0800

    Creates new file for JWT Authentication

---
+++ SimpleCRUDWebApp.Infastructure/Extensions/AuthenticationServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace DailyQuoteManager.Infrastructure.Extensions
+{
+   public static class AuthenticationServiceExtensions
+    {
+        #region Public Methods
+
+        public static IServiceCollection AddJwtAuthentication(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JWTSettings");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                 .AddJwtBearer(options =>
+                 {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuerSigningKey = true,
+                         IssuerSigningKey = new SymmetricSecurityKey(
+                             Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!)),
+                         ValidateIssuer = true,
+                         ValidateAudience = true,
+                         ValidateLifetime = true,
+                         ValidIssuer = jwtSettings["Issuer"],
+                         ValidAudience = jwtSettings["Audience"],
+                         ClockSkew = TimeSpan.Zero,
+                         RoleClaimType = ClaimTypes.Role
+                     };
+                 });
+            return services;
+
+        }
+            #endregion Public Methods
+
+        }
+}
