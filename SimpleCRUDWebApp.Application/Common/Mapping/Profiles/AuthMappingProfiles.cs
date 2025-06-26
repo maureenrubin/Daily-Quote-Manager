9484205a07ec248f345922bdf7c85bd0dd713cd0
maureenrubin <maureenrubin21@gmail.com>
2025-06-26 10:50:00 +0800

    Creates method for AutoMapper -ApplicationUser & RefreshToken

---
+++ SimpleCRUDWebApp.Application/Common/Mapping/Profiles/AuthMappingProfiles.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DailyQuoteManager.Application.DTOs.Auth.RefreshToken;
+using DailyQuoteManager.Application.DTOs.Auth.Register;
+using DailyQuoteManager.Domain.Entities;
+
+namespace DailyQuoteManager.Application.Common.Mapping.Profiles
+{
+    public class AuthMappingProfiles : Profile
+    {
+
+        #region Public Constructors
+
+        public AuthMappingProfiles()
+        {
+            CreateMap<RegisterUserInputRequestDto, ApplicationUser>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.ToLower().Trim()))
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
+          
+            CreateMap<RefreshTokenDto, RefreshToken>()
+                .ForMember(dest => dest.ExpiresAt, opt => opt.MapFrom(src => src.Expires))
+                .ForMember(dest => dest.ApplicationUser, opt => opt.Ignore()) 
+                .ForMember(dest => dest.AppUserId, opt => opt.Ignore()) 
+                .ForMember(dest => dest.RefreshTokenId, opt => opt.Ignore());
+        }
+
+        #endregion Public Constructors
+    }
+}
+ 
